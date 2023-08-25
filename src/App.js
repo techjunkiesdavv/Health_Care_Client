e7b18ca0637f68dad3c0aa5bda29fcc98b6d5fe7
Harsh Farkiya <108735661+HarshhFarkiya@users.noreply.github.com>
2023-08-25 21:27:03 +0530

    health care clinet with qrcode generation and qrcode scanning developed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import RegistrationForm from "./components/registrationform/RegistrationForm";
+
+function App() {
+  return (
+    <div>
+      <RegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/registrationform/RegistrationForm.js
@@ -0,0 +1,133 @@
+import React from 'react'
+import { useState } from 'react';
+import styles from "./RegistrationForm.module.scss";
+import QRCode from 'qrcode';
+import {QrReader} from 'react-qr-reader';
+function RegistrationForm() {
+    const [patientDetails,setPatientDetails] = useState({firstName:'',lastName:'',phone:'',alternate:'',address:''});
+    const [qrUrl,setUrl] = useState("");
+    const [data,setData] = useState('');
+    const handleChange=(e)=>{
+        setPatientDetails({...patientDetails,[e.target.name]:e.target.value});
+      };
+    const generateQrCode= async (e)=>{
+        e.preventDefault();
+        //const BaseUrl = "https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl="+"firstName:" + patientDetails.firstName+"lastName:"+patientDetails.lastName+"phone:"+patientDetails.phone+"alternte:"+patientDetails.alternate+"address:"+patientDetails.address;
+
+        console.log(patientDetails)
+        try{
+          const data =patientDetails.firstName+"&"+patientDetails.lastName+"&"+patientDetails.phone+"&"+patientDetails.alternate+"&"+patientDetails.address;
+          const qrcodeGenerated = await QRCode.toDataURL(JSON.stringify(patientDetails));
+          setUrl(qrcodeGenerated);
+        }
+        catch(err){
+          console.log(err);
+        }
+    }
+    const handleQrScan=(result)=>{
+      if(result){
+        setData(result.text);
+        console.log(result.text);
+      }
+    }   
+    const handleQrScanError=(err)=>{
+      console.log(err);
+    }
+     return (
+      <div>
+        <div className={styles.container}>
+          <div className={styles.border}>
+            <div className={styles.title}>Hello, Welcome to 'Hospital Name'!</div>
+            <form onSubmit={generateQrCode}>
+              <div className={styles.userdetails}>
+                <div className={styles.inputbox}>
+                  <input
+                    className={styles.firstName}
+                    type="text"
+                    placeholder="First Name"
+                    required
+                    name='firstName'
+                    value={patientDetails.firstName}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className={styles.lastName}
+                    type="text"
+                    placeholder="Last Name"
+                    required
+                    name='lastName'
+                    value={patientDetails.lastName}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className={styles.inputbox}>
+                  <input
+                    className={styles.phone}
+                    type="text"
+                    placeholder="Phone number"
+                    required
+                    name='phone'
+                    value={patientDetails.phone}
+                    onChange={handleChange}
+                  />
+                  <input
+                    className={styles.alternate}
+                    type="text"
+                    placeholder="Alternate phone number"
+                    required
+                    name='alternate'
+                    value={patientDetails.alternate}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className={styles.inputbox}>
+                  <input
+                    className={styles.wing}
+                    type="text"
+                    placeholder="Aadhar Number"
+                    required
+                    name='aadharNumber'
+                    value={patientDetails.address}
+                    onChange={handleChange}
+                  />
+                  
+                </div>
+                <div className={styles.inputbox}>
+                  <input
+                    className={styles.wing}
+                    type="text"
+                    placeholder="Address"
+                    required
+                    name='address'
+                    value={patientDetails.address}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className={styles.button}>
+                <button type="submit">Generate QR Code</button>
+              </div>
+            </form>
+            <div>
+              {qrUrl&&<a href={qrUrl} download={true}>
+                <img src={qrUrl}/>
+                </a>}
+                </div>
+                <QrReader
+          delay={300}
+          
+          onError={handleQrScanError}
+          onScan={handleQrScan}
+          onResult={(result,error)=>{
+handleQrScan(result);
+          }}  
+          />
+          <p>{data}</p>
+          </div>
+          
+        </div>
+      </div>
+    );
+}
+
+export default RegistrationForm
