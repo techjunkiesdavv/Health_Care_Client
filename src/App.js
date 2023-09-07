a64151b041d150e4307360cdc0952dfe0a9011a2
Harsh Farkiya <108735661+HarshhFarkiya@users.noreply.github.com>
2023-09-07 14:27:32 +0530

    error resolved

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
               <Facilities/>
               <AddHealthinfoForm />
               <RegistrationForm />
+              
               </>
               } />
             
