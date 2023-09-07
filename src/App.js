3f700e07826daae5db5ce646da8882133ef36ddd
Harsh Farkiya <108735661+HarshhFarkiya@users.noreply.github.com>
2023-09-07 14:23:35 +0530

    error resolved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import AddHealthinfoForm from "./components/AddHealth/AddHealthinfoForm";
-import BookAppointment from "./components/Appointment/BookAppointment";
-import Healthcare from "./components/HealthCareContent/HealthCare";
-import HealthcareContent from "./components/HealthCareContent/HealthcareContent";
-import GenerateOtp from "./components/Registration/GenerateOtp";
-import Hospital from "./components/Registration/Hospital";
-import LoginForm from "./components/Registration/LoginForm";
 import Patient from "./components/Registration/Patient";
 import RegistrationForm from "./components/registrationform/RegistrationForm";
-
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/Home/Home";
 import Facilities from "./components/Facilities/Facilities";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import "./App.scss";
+import Home from "./Pages/Home";
+import Legal from "./Pages/Legal";
+import NotFound from "./Pages/NotFound";
+import Appointment from "./Pages/Appointment";
 
 function App() {
   return (
   <BrowserRouter >
-      <Navbar/>
      <Routes>
             <Route
               path="/"
@@ -33,10 +27,12 @@ function App() {
               } />
             
               
-              <Route
+             <Route
               path="/patient"
               element={<Patient/>} />
-              
+          <Route path="/legal" element={<Legal />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="*" element={<NotFound />} />
               </Routes>
 
       
